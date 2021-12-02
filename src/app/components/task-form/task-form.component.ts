842d618b7051a8a3d8440a802f263fc0e7edc8d3
Mau <lammau123@gmail.com>
2021-12-02 23:34:08 +0700

    add validate ui

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
@@ -7,17 +7,28 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./task-form.component.css']
 })
 export class TaskFormComponent implements OnInit {
-  formGroup = new FormGroup({
-    scheduleName: new FormControl(''),
-    time: new FormControl('')
-  });
+  formGroup!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      scheduleName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)]),
+      time: new FormControl('', Validators.required)
+    });
   }
 
   onSubmit(){
-    console.log("on submit");
+    console.log(this.formGroup.controls['scheduleName'].errors?.['required']);
+  }
+
+  get scheduleName() {
+    return this.formGroup.get('scheduleName');
+  }
+
+  get time() {
+    return this.formGroup.get('time');
   }
 }
