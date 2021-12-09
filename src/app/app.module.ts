fb49b032248646aae8c5f65653159bea8c8d7727
Mau <lammau123@gmail.com>
2021-12-09 15:52:01 +0700

    add pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TaskManagerComponent } from './components/task-manager/task-manager.component';
 import { TaskComponent } from './components/task/task.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskManagerComponent,
     TaskComponent,
-    TaskFormComponent
+    TaskFormComponent,
+    PaginatorComponent
   ],
   imports: [
     BrowserModule,
